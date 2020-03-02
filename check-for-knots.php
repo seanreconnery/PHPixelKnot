78be5cbf03d340c308b4c9bdd27ac5b8069527c0
seanreconnery <56751730+seanreconnery@users.noreply.github.com>
2020-03-02 10:42:04 -0800

    improved performance

--- check-for-knots.php
+++ check-for-knots.php
@@ -1,83 +1,75 @@
-<?php 
+<?php
 
 $HeaderA = hex2bin('ffd8ffdb008400');
 $HeaderB = hex2bin('ffd8ffe000104a');
-$byteString1 = hex2bin('46494600010100000100010000ffdb00');
-$byteString2 = hex2bin('0302020302020303030304030304050805050404050a070706080c0a0c0c0b0a0b0b0d0e12100d0e110e0b0b1016101113141515150c0f171816141812141514');
+$HeaderC = hex2bin('ffd8ffe1001845');
+
+$byteString1 = hex2bin('46494600010100000100010000ffdb');
+
+$byteString2a = hex2bin('0302020302020303030304030304050805050404050a070706080c0a0c0c0b0a0b0b0d0e12100d0e110e0b0b1016101113141515150c0f171816141812141514');
+$byteString2b = hex2bin('0403030403030404030405040405060a07060606060d090a080a0f0d10100f0d0f0e11131814111217120e0f151c151719191b1b1b10141d1f1d1a1f181a1b1a');
+$byteString2c = hex2bin('49492a00080000000000000000000000ffec00114475636b79000100040000003c0000ffee000e41646f62650064c000000001ffdb0084000604040405040605');
+
+
 $pkseq = hex2bin('ffc0001108');
 $byteString4 = hex2bin('03012200021101031101');
-$byteString5 = hex2bin('ffda000c03');
+
+$byteString5a = hex2bin('ffda000c03');
+$byteString5b = hex2bin('09e18552cd');
+$byteString5c = hex2bin('fee5588e3f');
+$byteString5d = hex2bin('8a298c28a2');
+
 
 $filename = $argv[1];
 
 printf($argv[1]);
 print("\n\n");
 
-$crossing = 1 - strlen($pkseq); //  set a negative value to backtrack thru byte chunks if necessary
-$buflen = 128;
 
 $f = fopen($filename, "r") or die("Unable to open file!");
 $seq = fread($f, 7); // grab the first few bytes of the header
 
-if(strpos($seq, $HeaderA) !== False or strpos($seq, $HeaderB) !== False) 
+
+if (strpos($seq, $HeaderA) !== False or strpos($seq, $HeaderB) !== False or strpos($seq, $HeaderC) !== False)
 {
-    // found the jpeg header, parse thru the rest of the file
-    // look for the next set of bytes
-    if (strpos($seq, $HeaderA) !== False) {$split = False;}  // if the rest of the strings are found, this is will be ORIGINAL PIXEL KNOT header
-    if (strpos($seq, $HeaderB) !== False) {$split = True;}   // if the rest of the strings are found, this will be MODIFIED PIXEL KNOT header
-    
-    if ($split == True) {
-        $seq .= fread($f, 18);
-        // look for 18 byte string -- this could indicate a potential modified PixelKnot header
-        // If Split is NOT true, but we still find all the other strings, then it means a probable (original) PixelKnot header
+    // found the header, so lets read in the whole file, and then check for the various byte strings that are present in PK
+    $seq = fread($f, filesize($filename));
+    print($filename);
+    print("\n");
 
-        if (strpos($seq, $byteString1) !== False) {
-            // found the 18 byte string, go onto the next part
+    if (strpos($seq, $byteString2a) !== False or strpos($seq, $byteString2b) !== False or strpos($seq, $byteString2c) !== False) {
+        // found the next set of bytes
+        
+        if (strpos($seq, $pkseq) !== False) {
+            // found PK string
+            
+            if (strpos($seq, $byteString4) !== False) {
+                // found 10 byte string
+                
+                if (strpos($seq, $byteString5a) !== False or strpos($seq, $byteString5b) !== False or strpos($seq, $byteString5c) !== False or strpos($seq, $byteString5d) !== False) {
+                    # found final 5 bytes
+                    print("PIXELKNOT indications foumd");
+                } else {
+                    # not found
+                    
+                }
+            } else {
+                # not found
+                
+            }
         } else {
-            // didn't find the 18 bytes, this probably isn't a stego'ed image file (at least not with PixelKnot)
-            exit;
+            # not found
+            
         }
-    }
-        
-    $seq .= fread($f, $buflen);
-    if(strpos($seq, $byteString2)) 
-    {
-        // still finding the correct byte strings, so lets keep going
-        $seq .= fread($f, $buflen);
-        if(strpos($seq, $pkseq)) 
-        {
-            // on to the next one.. 
-            $seq .= fread($f, 32);
-            if(strpos($seq, $byteString4)) 
-            { 
-                    //final 5 byte string
-                    while(!feof($f)) 
-                        {
-                        $seq .= fread($f, 10);
-                            if(strpos($seq, $byteString5) === false) // strict compare (although you've already checked the 0 pos byte for jpg header)
-                            // since this can potentially return 0, best practice I guess is use strict compare.
-                            {
-                                // keep last n-1 bytes, because they can be beginning of required sequence
-                                $seq = substr($seq, $crossing);
-                            }
-                            else
-                            {
-                                // PIXEL KNOT FILE -- modified or original headers though?
-                                if ($split) {
-                                    echo $argv[1] . " -- (modified) PixelKnot header detected";
-                                } else {
-                                    echo $argv[1] . " -- PixelKnot header detected";
-                                }                                    
-                                    break;
-                                }
-                            }
-                        }
-
-            } 
+    } else {
+        # not found
         
     }
 
-}
+    print("No indications of PixelKnot found");
+
+}    
+
 unset($seq);    // clear the string from memory
 
 ?>
